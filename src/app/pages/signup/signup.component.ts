f6f0e07df0b572d5f1a45febfbef85d5b0d5ef80
Konstantin Myrsaitov <myrsaitov@gmail.com>
2020-04-15 16:09:48 +0300

    signup

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountService} from '../../services/account.service';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import {confirmPasswordValidator} from '../../directives/confirm-password-validator.directive';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +9,49 @@ import {Component} from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder, private accountService: AccountService) {
+  }
+
+  ngOnInit() {
+    const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[+!@#$%^&*]).{6,20}/g;
+
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20), Validators.pattern(pattern)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]]
+    }, {validators: confirmPasswordValidator});
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get firstName() {
+    return this.form.get('firstName');
+  }
+
+  get lastName() {
+    return this.form.get('lastName');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+  get confirmPassword() {
+    return this.form.get('confirmPassword');
+  }
+
+  register() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.accountService.register(this.form.value).subscribe(res => console.log(res));
+  }
 }
