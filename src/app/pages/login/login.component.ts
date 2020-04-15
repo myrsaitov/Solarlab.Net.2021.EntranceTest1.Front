8a98e658f2b70478c22e5b3f40a59f397c1610bc
Konstantin Myrsaitov <myrsaitov@gmail.com>
2020-04-15 16:10:19 +0300

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ILogin} from 'src/app/models/account/login.model';
+import {BaseService} from 'src/app/services/base.service';
+import {ApiUrls} from 'src/app/shared/apiURLs';
+import {Router} from '@angular/router';
+import {AuthService} from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
 
-  constructor() {
+  private formObj = {
+    email: [null, [Validators.required, Validators.email]],
+    password: [null, [Validators.required, Validators.min(8)]],
+    rememberMe: [false]
+  };
+
+  constructor(
+    private readonly baseService: BaseService,
+    private readonly router: Router,
+    private readonly auth: AuthService,
+    private fb: FormBuilder,
+  ) {
+    this.loginForm = fb.group(this.formObj);
   }
 
   ngOnInit() {
-    
+
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  public async login() {
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.invalid) {
+      return;
+    }
+    const payload: ILogin = this.loginForm.getRawValue();
+    await this.baseService.post(ApiUrls.login, payload)
+      .then(res => {
+        if (res) {
+          this.auth.saveSession(res);
+          this.router.navigate(['/']);
+        }
+      });
   }
 }
